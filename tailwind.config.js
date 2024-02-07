0f5250acfb4a57f3a76ef0898283c4633dab1bd6
OS X <rishabhdhawan@icloud.com>
2024-02-07 16:42:18 +0530

    done laptop design for images caraousel and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      kumbh: ["Kumbh Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        orange: "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25, 100%, 94%)",
 
-    colors: {
-      orange: "hsl(26, 100%, 55%)",
-      "pale-orange": "hsl(25, 100%, 94%)",
-
-      "very-dark-blue": "hsl(220, 13%, 13%)",
-      "dark-grayish-blue": "hsl(219, 9%, 45%)",
-      "grayish-blue": "hsl(220, 14%, 75%)",
-      "light-grayish-blue": "hsl(223, 64%, 98%)",
-      white: "hsl(0, 0%, 100%)",
-      black: "hsl(0, 0%, 0%)",
-      "divider-color": "rgb(235, 235, 237)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+        black: "hsl(0, 0%, 0%)",
+        "divider-color": "rgb(235, 235, 237)",
+      },
     },
 
     screens: {
       tablet: "800px",
       laptop: "1024px",
+      "laptop-xl": "1440px",
     },
   },
   plugins: [],
